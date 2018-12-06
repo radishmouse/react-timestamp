a16af954cc00aef8146ecafc698bd4a31d585779
Chris Aquino <chris.aquino@gmail.com>
2018-12-06 10:51:16 -0500

    makes the button add dates to this.state.dates

--- src/App.js
+++ src/App.js
@@ -5,26 +5,52 @@ import './App.css';
 import DateList from './DateList';
 
 const someDates = [
-  '2018-12-06 09:21:21',
-  '2018-12-06 09:21:21',
-  '2018-12-06 09:21:21',
-  '2018-12-06 09:21:21',
   '💩',
-  '2018-12-06 09:21:21',
   '2018-12-06 09:21:21',
   '2018-12-06 09:27:21'
 ];
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dates: [
+        '💩',
+        '💩',
+        '💩',
+      ]
+    };
+  }
   render() {
 
     return (
       <div className="main-component">
-        <button className="my-button">go ahead, click me, i dare you</button>
+        <button 
+          onClick={() => { 
+            console.log('ouch.');
+            const aNewDate = new Date();
+            this.setState({
+              // replace the value of 'dates'
+              // with a *new* array
+              dates: [
+                // it should contain
+                // all the existing values from this.state.dates
+                ...this.state.dates,
+
+                // but also!
+                // our new date object as a string
+                aNewDate.toLocaleString()
+              ]
+            });
+            // someDates.push(aNewDate);
+            // console.log(someDates);
+          }} 
+          className="my-button"
+        >go ahead, click me, i dare you</button>
 
         <DateList 
-          listOfDates={someDates}
-
+          listOfDates={this.state.dates}
         /> 
 
       </div>
--- src/DateList.js
+++ src/DateList.js
@@ -8,7 +8,7 @@ import React from 'react';
 
 function convertDatesToItems(someDates) {
     const datesAsListItems = someDates.map((dateString) => {
-        return (<li>{dateString}</li>);
+        return (<li>{dateString}!!!!!!</li>);
     }); 
     return datesAsListItems;   
 }
